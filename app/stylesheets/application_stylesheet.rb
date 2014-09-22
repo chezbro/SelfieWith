577b108a74ba99d5be6cbe5df33ccc1e48119a69
chunlea <ichunlea@me.com>
2014-09-22 21:31:23 +0800

    Update the application_stylesheet.rb

--- app/stylesheets/application_stylesheet.rb
+++ app/stylesheets/application_stylesheet.rb
@@ -19,10 +19,30 @@ class ApplicationStylesheet < RubyMotionQuery::Stylesheet
     font.add_named :large,    font_family, 36
     font.add_named :medium,   font_family, 24
     font.add_named :small,    font_family, 18
+    font.add_named :msmall,   font_family, 16
+    font.add_named :xsmall,   font_family, 14
 
     color.add_named :tint, '236EB7'
     color.add_named :translucent_black, color.from_rgba(0, 0, 0, 0.4)
     color.add_named :battleship_gray,   '#7F7F7F'
+    color.add_named :bg_black,          '#303134'
+    color.add_named :darker_black,      '#1E1E1E'
+    color.add_named :gray_black,        '#2A2A2A'
+    color.add_named :nav_text,          '#999995'
+
+    # UINavigationBar.appearance.setBarTintColor color.bg_black
+    UINavigationBar.appearance.setBarStyle UIBarStyleBlack
+    UINavigationBar.appearance.setTitleTextAttributes({
+      # UITextAttributeFont => UIFont.fontWithName('Trebuchet MS', size:24),
+      UITextAttributeTextShadowColor => color.clear,
+      UITextAttributeTextColor       => color.nav_text
+    })
+    UINavigationBar.appearance.setShadowImage UIImage.alloc.init
+    # UITabBar.appearance.setBarTintColor color.bg_black
+    UITabBar.appearance.setBarStyle UIBarStyleBlack
+    UITabBar.appearance.setSelectionIndicatorImage image.resource('tabBarItem_selected')
+    UITabBar.appearance.setSelectedImageTintColor color.white
+    UIToolbar.appearance.setBarStyle UIBarStyleBlack
   end
 
   def standard_button(st)
@@ -37,4 +57,26 @@ class ApplicationStylesheet < RubyMotionQuery::Stylesheet
     st.color = color.black
   end
 
+  def text_field(st)
+    st.frame                   = {l: 20, fr:20, h: 45}
+    st.background_color        = color.darker_black
+    st.view.font               = font.msmall
+    st.text_alignment          = :center
+    st.corner_radius           = 0
+    st.text_color              = color.white
+    st.clear_button_mode       = UITextFieldViewModeWhileEditing
+    # st.view.color            = color.white
+    # st.view.setKeyboardType(UIKeyboardTypeEmailAddress)
+    # st.reloadInputViews
+    st.view.keyboardAppearance = UIKeyboardAppearanceDark
+    st.view.setValue(color.bg_black, forKeyPath:"_placeholderLabel.textColor")
+    clearButton = st.view.valueForKey("_clearButton")
+    clearButton.setImage(image.resource('clear_btn'), forState:UIControlStateNormal)
+  end
+  def text_field_label(st)
+    st.frame          = {l:0, t:0, w: 80, h: 45, centered: :horizontal}
+    st.text_alignment = :center
+    st.color          = color.nav_text
+    st.font           = font.xsmall
+  end
 end
