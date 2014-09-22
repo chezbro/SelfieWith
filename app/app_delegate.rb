dd215e9db86295b0cecb12aadb78d04a202a2336
chunlea <ichunlea@me.com>
2014-09-22 21:25:20 +0800

    Add Server method to update the AFMotion data and setup the logout method

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,7 @@ class AppDelegate
   attr_reader :window
 
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    server
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
     main_controller = MainController.new
@@ -10,4 +11,26 @@ class AppDelegate
     @window.makeKeyAndVisible
     true
   end
+
+  def server
+    # UIApplication.sharedApplication.delegate.server
+    if Auth.needlogin?
+      AFMotion::Client.build_shared(BASE_URL) do
+        header "Accept", "application/json"
+        response_serializer :json
+      end
+    else
+      AFMotion::Client.build_shared(BASE_URL) do
+        header "Accept", "application/json"
+        header "X-Api-Username",  Auth.username
+        header "X-Api-Token",  Auth.token
+        response_serializer :json
+      end
+    end
+  end
+  def logout
+    Auth.reset
+    open_welcome_controller
+  end
+
 end
