8e87acd9314f4b451705747f89f03b98be07f147
chunlea <ichunlea@me.com>
2014-10-04 17:07:01 +0800

    Add get method for API wrapper

--- app/shared/api.rb
+++ app/shared/api.rb
@@ -36,5 +36,41 @@ class API
         end
       end
     end
+    def get(route, params={}, &block)
+      UIApplication.sharedApplication.networkActivityIndicatorVisible = true
+      AFMotion::Client.shared.get(route, params) do |result|
+        UIApplication.sharedApplication.networkActivityIndicatorVisible = false
+        if result.success?
+          block.call(result.object) if block
+        elsif result.object && result.operation.response.statusCode.to_s =~ /401/
+          UIApplication.sharedApplication.delegate.logout
+          SimpleSI.alert("Your seesion are expired, please try relogin.")
+        elsif result.object && result.operation.response.statusCode.to_s =~ /40\d/
+          error = "There is something wrong."
+          if result.object["message"].kind_of?(String)
+            error = result.object["message"].to_s
+          else
+            error = result.object["message"].map { |e| e.join(" ") }.join(", ")
+          end
+          SimpleSI.alert({
+            message: error.capitalize,
+            transition: "bounce",
+            buttons: [
+              {title: "Got it", type: "cancel"} # action is secondary
+            ]
+          })
+          block.call(nil) if block
+        else
+          SimpleSI.alert({
+            message: result.error.localizedDescription,
+            transition: "bounce",
+            buttons: [
+              {title: "Got it", type: "cancel"} # action is secondary
+            ]
+          })
+          block.call(nil) if block
+        end
+      end
+    end
   end
 end
